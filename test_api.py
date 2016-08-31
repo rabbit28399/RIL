94ad123dbe97e6eab523c79f52971c9f8f938ed5
Vonny Pawaka <vpawaka@wks-15-217>
2016-08-31 15:07:07 +0200

    new update

--- test_api.py
+++ test_api.py
@@ -1,21 +1,35 @@
 import re
 import en
 from google_ngram_downloader import readline_google_store
-all_records = readline_google_store(ngram_len=1, indices =  'z')
+from nltk.corpus import words
+all_records = readline_google_store(lang='eng', ngram_len=1, indices='a')
+this_count = 0
 
 pattern = re.compile('_(VERB|VP)$')
 for (fname, url, records) in all_records:
     for r in records:
-        ngram=r.ngram
-        grams=ngram.split()
+        ngram = r.ngram
+        grams = ngram.split()
         flag = False
+        checker = False
         for gram in grams:
             if pattern.search(gram):
-                flag = True
-                pp_list = []
-                gram_word = ngram.split("_")[0]
-                #pp = en.verb.past_participle('"%s"'%gram_word)
-                #pp_list.append(pp)
-                #print pp_list
+                gram_word = gram.split("_")[0]
+                low = str("%s" % gram_word).lower()
+                if low in words.words():
+                    flag = True
+                    checker = True
+                else:
+                    print (low, "Not an English Word")
                 break
-        if flag: print(ngram)
+
+        if flag:
+            this_count += r.match_count
+            print(gram_word)
+            print(this_count)
+
+        pp_list = []
+        if checker:
+            pp = en.verb.past_participle(low)
+            pp_list.append(pp)
+            print "PP LIST = ", pp_list
