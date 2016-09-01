b203c540f47056bc7069dd2306b36c4bbb51f5b9
vonnyp <vonnypwk@gmail.com>
2016-09-01 13:11:15 +0200

    Update

--- test_api.py
+++ test_api.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import re
 import en
 from google_ngram_downloader import readline_google_store
 from nltk.corpus import words
-all_records = readline_google_store(lang='eng', ngram_len=1, indices='a')
+from nltk.stem.wordnet import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+all_records = readline_google_store(lang='eng', ngram_len=1, indices='z')
+this_ngram = "*"
 this_count = 0
 
 pattern = re.compile('_(VERB|VP)$')
@@ -15,21 +21,24 @@ for (fname, url, records) in all_records:
         for gram in grams:
             if pattern.search(gram):
                 gram_word = gram.split("_")[0]
-                low = str("%s" % gram_word).lower()
-                if low in words.words():
+                gram_word_unicode = gram_word.encode('utf-8')
+                low = str("%s" % gram_word_unicode).lower()
+                gram_lemma = lemmatizer.lemmatize(low, 'v')
+                if gram_lemma in words.words():
                     flag = True
                     checker = True
-                else:
-                    print (low, "Not an English Word")
                 break
 
         if flag:
-            this_count += r.match_count
-            print(gram_word)
-            print(this_count)
+            if (gram_lemma == this_ngram):
+                this_count += r.match_count
+            else:
+                print this_ngram, "\t", this_count
+                this_ngram = gram_lemma
+                this_count = r.match_count
 
-        pp_list = []
-        if checker:
-            pp = en.verb.past_participle(low)
-            pp_list.append(pp)
-            print "PP LIST = ", pp_list
+        #pp_list = []
+        #if checker:
+            #pp = en.verb.past_participle(low)
+            #pp_list.append(pp)
+            #print "PP LIST = ", pp_list
