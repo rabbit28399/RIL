9908010bb42c7a6cefb4702c2e95f7281886a3ae
vonnyp <vonnypwk@gmail.com>
2016-09-06 14:23:57 +0200

    message

--- try.py
+++ try.py
@@ -1,26 +1,3 @@
-import re
-import en
-from nltk.corpus import words
-pattern = re.compile('_(VERB|VP)$')
-
-with open('coba') as f:
-    for line in f:
-        ngram = line.split("\t")[0]
-        grams = ngram.split()
-        flag = False
-        checker = False
-        for gram in grams:
-            if pattern.search(gram):
-                gram_word = gram.split("_")[0]
-                low = str("%s"%gram_word).lower()
-                if low in words.words():
-                    checker = True
-                else:
-                    print (low, "Tidak")
-                break
-
-        pp_list = []
-        if checker:
-            pp = en.verb.past_participle(low)
-            pp_list.append(pp)
-            print pp_list
+a = ['Saya', 'Kamu', 'Dia']
+for pron in a:
+    print pron
